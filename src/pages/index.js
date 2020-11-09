e703bc98875fb83a0489673e6016a0ba744e599e
Daniel malagurti <danielmalagurti@gmail.com>
2020-11-09 19:18:57 -0300

    refactor: :lipstick: change home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,33 +2,36 @@ import React from "react"
 import Navbar from "../components/App/Navbar"
 import Footer from "../components/App/Footer"
 import Layout from "../components/App/Layout"
-import Banner from "../components/Index/Banner"
-import OurSolutions from "../components/Index/OurSolutions"
-import OurServices from "../components/Index/OurServices"
-import OurFeatures from "../components/Index/OurFeatures"
-import TeamMember from "../components/Index/TeamMember"
-import RecentProjects from "../components/Index/RecentProjects"
-import Pricing from "../components/Index/Pricing"
-import Testimonials from "../components/Index/Testimonials"
-import Partner from "../components/Index/Partner"
-import ProjectStartArea from "../components/Index/ProjectStartArea"
-import OurBlog from "../components/Index/OurBlog"
+import MainBanner from "../components/DataAnalyticsAIStartup/MainBanner"
+import FeaturedService from "../components/DataAnalyticsAIStartup/FeaturedService"
+import AboutUs from "../components/DataAnalyticsAIStartup/AboutUs"
+import OurMission from "../components/DataAnalyticsAIStartup/OurMission"
+import Funfacts from "../components/DataAnalyticsAIStartup/Funfacts"
+import OurServices from "../components/DataAnalyticsAIStartup/OurServices"
+import HowItWork from "../components/DataAnalyticsAIStartup/HowItWork"
+import TeamMembers from "../components/DataAnalyticsAIStartup/TeamMembers"
+import Testimonials from "../components/DataAnalyticsAIStartup/Testimonials"
+import Partner from "../components/DataAnalyticsAIStartup/Partner"
+import StartProject from "../components/DataAnalyticsAIStartup/StartProject"
+import OurBlog from "../components/DataAnalyticsAIStartup/BlogPost"
 
 const Home = () => {
   return (
     <Layout>
       <Navbar />
-      <Banner />
-      <OurSolutions />
+      <MainBanner />
+      <FeaturedService />
+      <AboutUs />
+      <OurMission />
+      <Funfacts />
       <OurServices />
-      <OurFeatures />
-      <TeamMember />
-      <RecentProjects />
-      <Pricing />
+      <HowItWork />
+      <TeamMembers />
       <Testimonials />
       <Partner />
+      <StartProject />
       <OurBlog />
-      <ProjectStartArea />
+      
       <Footer />
     </Layout>
   )
